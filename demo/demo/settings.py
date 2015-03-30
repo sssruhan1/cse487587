09043f16f500c64505a15aaee1cef33075f0f511
Ruhan Sa <ruhansa@dhcp035-164-153.wireless.buffalo.edu>
2015-03-30 16:23:23 -0400

    scrapy

---
+++ demo/demo/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for demo project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'demo'
+
+SPIDER_MODULES = ['demo.spiders']
+NEWSPIDER_MODULE = 'demo.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'demo (+http://www.yourdomain.com)'
